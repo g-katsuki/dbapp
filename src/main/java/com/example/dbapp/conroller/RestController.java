2079014c64e0b9093906afdcb62ccc6dcbab4e27
Go Katsuki <go.katsuki@rakuten.com>
2023-09-29 13:05:46 +0900

    add index template

--- src/main/java/com/example/dbapp/conroller/RestController.java
+++ src/main/java/com/example/dbapp/conroller/RestController.java
@@ -2,14 +2,26 @@ package com.example.dbapp.conroller;
 
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+    int a = 0;
 
     @RequestMapping("/")
     public String test(){
         return "test";
+    }
+
+    @GetMapping(value = {"/index", ""})
+    public ModelAndView index() {
+        a++;
+        ModelAndView model = new ModelAndView("index");
+        model.addObject("message", "ハローワールド!");
+        model.addObject("num", a);
+        return model;
     }
 }
