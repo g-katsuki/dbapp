0297131c9927f195ed1817ec04c1fdd2ddf0866f
Go Katsuki <go.katsuki@rakuten.com>
2023-09-29 18:03:31 +0900

    success conent to DB

--- src/main/java/com/example/dbapp/conroller/RestController.java
+++ src/main/java/com/example/dbapp/conroller/RestController.java
@@ -1,15 +1,23 @@
 package com.example.dbapp.conroller;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import com.example.dbapp.model.*;
+import com.example.dbapp.mapper.*;
 
 @org.springframework.web.bind.annotation.RestController
+@MapperScan(basePackages = {"com.example.dbapp"})
 public class RestController {
     int a = 0;
+
+    @Autowired
+    ProductMapper productMapper;
 
     @RequestMapping("/")
     public String test(){
@@ -24,4 +32,11 @@ public class RestController {
         model.addObject("num", a);
         return model;
     }
+
+    @RequestMapping("/map")
+    public String map(){
+        Product product = productMapper.selectByPrimaryKey(2);
+        return product.getName();
+    }
+
 }
